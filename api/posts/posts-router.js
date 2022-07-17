aa29c2f17a365053840c735ce2d15f8eae51ef33
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-16 21:14:49 -0500

    adds get by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,51 @@
 // implement your posts router here
 const express = require('express')
-
+const Post = require('./posts-model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    Post.find()
+    .then(found=> {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message, 
+            stack: err.stack
+        })
+    })
+})
+router.get('/:id', async (req, res) => {
+    try {
+        const selectedPost = await Post.findById(req.params.id)
+        if(!selectedPost) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.json(selectedPost)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message, 
+            stack: err.stack
+        })
+    }
+})
+router.post('/', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/comments', (req, res) => {
+
+})
+
 
 module.exports = router
