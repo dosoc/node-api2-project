c49cadd7e2713d790da0e803206cc0036afaf361
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-16 22:00:51 -0500

    finish mvp

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -100,7 +100,9 @@ router.put('/:id', (req, res) => {
                 }
             })
             .then(post => {
-                res.json(post)
+                if(post) {
+                   res.json(post) 
+                }
             })
             .catch(err=>{
                 res.status(500).json({
@@ -111,8 +113,24 @@ router.put('/:id', (req, res) => {
             })
     }
 })
-router.get('/:id/comments', (req, res) => {
-
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const selectedPost = await Post.findById(req.params.id)
+        if (!selectedPost) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message, 
+            stack: err.stack
+        })
+    }
 })
 
 
